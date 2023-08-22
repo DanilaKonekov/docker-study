7e656cd2b2c268aa531375a472b5cdfac2525d16
Коньков Данила <danila9807@ya.ru>
2023-08-22 10:32:46 +0300

    init

---
+++ test/database/seeders/SourceItemSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SourceItemSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
