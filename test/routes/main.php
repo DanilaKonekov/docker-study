878f5204658d2d94f0df39f8e99a4d6e9e846ded
Коньков Данила <danila9807@ya.ru>
2023-08-28 12:20:49 +0300

    Создаем контроллеры

--- test/routes/main.php
+++ test/routes/main.php
@@ -8,28 +8,8 @@ use App\Http\Controllers\Posts\CommentController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::view('/', 'welcome');
-
-Route::redirect('/home', '/');
-
-// Сначала команду в контейнере для создания новго контролера php artisan make:controller TestController
-Route::get('/test', \App\Http\Controllers\TestController::class);
-
-Route::get('register', [RegisterController::class, 'index'])->name('register');
-Route::post('register', [RegisterController::class, 'store'])->name('register.store');
-
-Route::get('login', [LoginController::class, 'index'])->name('login');
-Route::post('login', [LoginController::class, 'store'])->name('login.store');
-Route::get('login/{user}/confirmation', [LoginController::class, 'confirmation'])->name('login.confirmation');
-Route::post('login/{user}/confirm', [LoginController::class, 'confirm'])->name('login.confirm');
-
-
-
-//Route::resources('posts',PostController::class)->only([
-//  'index', 'show',
-//]);
-
-Route::resource('posts/{post}/comments', CommentController::class);
+Route::resource('product_cards', ProductCardController::class);
+Route::resource('source_items', SourceItemController::class);
 
 
 Route::get('/product-cards', 'ProductCardController@index');
